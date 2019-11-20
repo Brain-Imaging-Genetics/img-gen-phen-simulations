78fe0b7436758cb47b10e7002422f97ffd0a5001
Elizabeth DuPre <emd222@cornell.edu>
2019-11-20 17:31:38 -0500

    enh: initial commit of generate_sex

--- orign_sim/sim_pheno.py
+++ orign_sim/sim_pheno.py
@@ -2,4 +2,24 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 
-import scipy
+from numpy.random import choice
+
+def generate_sex(size=10,
+                 probabilities=[0.5, 0.5]):
+    """
+    Parameters
+    ----------
+    size : int
+        The number of observations to return
+    probabilities : np.array
+        The relative probability of each sex.
+        By default, sexes are assumed to have equal
+        probability
+    
+    Returns
+    -------
+    np.array
+        Phenotype of genders following the
+        provided probabilities
+    """
+    return choice(2, size, p=probabilities)
